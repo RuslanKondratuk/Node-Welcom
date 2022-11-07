7bccb9249eb9519f51176a01690c4b29c3af6c71
ruslan <ruslan.kondratuk46@gmail.com>
2022-11-07 19:38:37 +0200

    Node-Welcom-2

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-console.log('Hello, Node');
 
 
-function sum() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-    return sum;
-}
 
-console.log(sum(1,2,3,4,5));
+// function sum() {
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+
+const sum = (...rest) => rest.reduce((acc,el) => acc + el, 0)
+
+console.log(sum(1,2,3,4,20));
